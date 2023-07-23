3132715b6d8436664852669803903481a584bf04
ayzatmalov <ayzat.malov@gmail.com>
2023-07-23 20:08:53 +0300

    adding the handler for show available currencies for conversion

--- main.py
+++ main.py
@@ -17,8 +17,16 @@ bot = telebot.TeleBot(TOKEN)
 # base commands:
 @bot.message_handler(commands=['start', 'help']) # use method message_handler with two arguments 'start' and 'help' or types of messages and return decorator
 def start(message: telebot.types.Message): # in decarator our function - start, which process the messages
-    text = 'Greetings!'
+    text = 'Hello Marat Malov!'
     bot.send_message(message.chat.id, text)
+
+# the command for handler which show availbale currencies for conversion:
+@bot.message_handler(commands=['values'])
+def values(message: telebot.types.Message):
+    text = 'available currencies:'
+    for i in major_exchanges.keys(): # go through the keys of major_currencies
+        text = '\n'.join((text, i)) # display each currency on new line
+    bot.reply_to(message, text) # attaching the message like a reply for user's message
 
 # launch communication with Telegram (use the method - polling, for start bot mode)
 bot.polling()
