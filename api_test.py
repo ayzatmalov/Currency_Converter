a41cd0d830b1e741171913d8d0ba8c93cab1620a
ayzatmalov <ayzat.malov@gmail.com>
2023-07-24 19:50:04 +0300

    creation and testing API with service cryptocompare

---
+++ api_test.py
@@ -0,0 +1,11 @@
+import requests
+import json
+
+base = "USD"
+quote = "RUB"
+amount = 100
+
+r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={base}&tsyms={quote}')
+resp = json.loads(r.content)
+price = resp[quote] * amount
+print(price) # 9081,00 (19:49 2023-07-24)
